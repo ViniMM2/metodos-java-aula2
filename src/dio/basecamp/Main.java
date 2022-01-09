1f629fa78bc8ce050ac409306716936d07f343c2
Vini Menezes Machado <drvinimach@gmail.com>
2022-01-09 17:19:31 -0300

    first commit

---
+++ src/dio/basecamp/Main.java
@@ -0,0 +1,14 @@
+package dio.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5D, 5D);
+        Quadrilatero.area(9, 8, 7);
+        Quadrilatero.area(5f, 5f);
+    }
+}
